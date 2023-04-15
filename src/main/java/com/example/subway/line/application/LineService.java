8b6ae64f7068557efed331e5e6f2e9ce872059fe
JungWoo <rlawjddn103@naver.com>
2023-04-15 16:39:00 +0900

    refactor: 의존성 주입 변수 접근 제어자 변경

--- src/main/java/com/example/subway/line/application/LineService.java
+++ src/main/java/com/example/subway/line/application/LineService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class LineService {
 
-    LineRepository lineRepository;
+    private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
--- src/main/java/com/example/subway/line/ui/LineController.java
+++ src/main/java/com/example/subway/line/ui/LineController.java
@@ -4,7 +4,6 @@ import com.example.subway.line.application.LineService;
 import com.example.subway.line.dto.LineRequest;
 import com.example.subway.line.dto.LineResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,7 @@ import java.net.URI;
 
 @RestController
 public class LineController {
-    LineService lineService;
+    private final LineService lineService;
 
     public LineController(LineService lineService) {
         this.lineService = lineService;
