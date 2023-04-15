f974bbf68bc8c252e1915ffec4a79a9f4d8c76bf
JungWoo <rlawjddn103@naver.com>
2023-04-15 14:51:23 +0900

    test(LineAcceptionTest): 지하철 노선 생성 테스트 구현

---
+++ src/test/java/com/example/subway/line/LineAcceptanceTest.java
@@ -0,0 +1,107 @@
+package com.example.subway.line;
+
+import com.example.subway.AcceptanceTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("지하철 노선 관련 기능")
+public class LineAcceptanceTest extends AcceptanceTest {
+    @DisplayName("지하철 노선을 생성한다.")
+    @Test
+    void createLine() {
+        // when
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "강남역");
+        params.put("color","bg-red-600");
+
+        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        // 지하철_노선_생성됨
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
+    @Test
+    void createLine2() {
+        // given
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_생성_요청
+
+        // then
+        // 지하철_노선_생성_실패됨
+    }
+
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void getLines() {
+        // given
+        // 지하철_노선_등록되어_있음
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_목록_조회_요청
+
+        // then
+        // 지하철_노선_목록_응답됨
+        // 지하철_노선_목록_포함됨
+    }
+
+    @DisplayName("지하철 노선을 조회한다.")
+    @Test
+    void getLine() {
+        // given
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_조회_요청
+
+        // then
+        // 지하철_노선_응답됨
+    }
+
+    @DisplayName("지하철 노선을 수정한다.")
+    @Test
+    void updateLine() {
+        // given
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_수정_요청
+
+        // then
+        // 지하철_노선_수정됨
+    }
+
+    @DisplayName("지하철 노선을 제거한다.")
+    @Test
+    void deleteLine() {
+        // given
+        // 지하철_노선_등록되어_있음
+
+        // when
+        // 지하철_노선_제거_요청
+
+        // then
+        // 지하철_노선_삭제됨
+    }
+}
