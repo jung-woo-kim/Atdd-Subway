4c28ad1b12dc271ae2b29331fb7ed620002093e4
JungWoo <rlawjddn103@naver.com>
2023-05-14 11:34:21 +0900

    feat: ExceptionHandler 예외처리 추가

--- src/main/java/com/example/subway/line/domain/Sections.java
+++ src/main/java/com/example/subway/line/domain/Sections.java
@@ -1,9 +1,6 @@
 package com.example.subway.line.domain;
 
-import com.example.subway.line.exception.section.SectionDuplicateException;
-import com.example.subway.line.exception.section.SectionMinimumSizeException;
-import com.example.subway.line.exception.section.SectionNotExistedException;
-import com.example.subway.line.exception.section.SectionNotLastStationException;
+import com.example.subway.line.exception.section.*;
 import com.example.subway.station.domain.Station;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embeddable;
@@ -34,7 +31,14 @@ public class Sections {
     }
 
     private void changeMatchedDownSection(Section section, Section matchedSection) {
+        validateMatchedSection(section, matchedSection);
         matchedSection.setDownStation(section.getUpStation());
+    }
+
+    private void validateMatchedSection(Section section, Section matchedSection) {
+        if (section.getDistance() >= matchedSection.getDistance()) {
+            throw new SectionDistanceExceedException();
+        }
     }
 
     private void changeMatchedUpSection(Section section, Section matchedSection) {
--- src/main/java/com/example/subway/line/exception/section/SectionExceptionType.java
+++ src/main/java/com/example/subway/line/exception/section/SectionExceptionType.java
@@ -5,9 +5,10 @@ import org.springframework.http.HttpStatus;
 
 public enum SectionExceptionType implements BaseExceptionType {
     SECTION_DUPLICATE(1001, HttpStatus.BAD_REQUEST.value(), "이미 해당하는 상행역, 하행역이 존재합니다."),
-    SECTION_NOT_EXIST(1002,HttpStatus.NOT_FOUND.value(), "상행역 하행역이 존재하지 않습니다."),
+    SECTION_NOT_EXIST(1002, HttpStatus.NOT_FOUND.value(), "상행역 하행역이 존재하지 않습니다."),
     SECTION_NOT_LAST_STATION(1003, HttpStatus.BAD_REQUEST.value(), "마지막 구간이 아닙니다."),
-    SECTION_MINIMUM_SIZE(1004, HttpStatus.BAD_REQUEST.value(), "구간이 현재 최소 크기입니다.");
+    SECTION_MINIMUM_SIZE(1004, HttpStatus.BAD_REQUEST.value(), "구간이 현재 최소 크기입니다."),
+    SECTION_DISTANCE_EXCEED(1005, HttpStatus.BAD_REQUEST.value(), "구간의 사이 거리가 새롭게 들어오는 구간의 거리보다 작습니다.");
 
     private final int errorCode;
     private final int httpStatus;
